aa0c0a0f49000c3f9d7406477999322106708b76
connorLuca <connormccrum7@gmail.com>
2020-09-08 16:01:03 -0600

    complete

--- ChangeMakerC/main.c
+++ ChangeMakerC/main.c
@@ -12,22 +12,53 @@
  */
 
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <cmath>
 /*
  * 
  */
 int main(int argc, char** argv) {
-    float toConvert;
+    float toConvert;//the dollar value owed to be converted
     puts("Enter the dollar value you need to broken down into change");
-    scanf("%i", toConvert);
+    scanf("%f", toConvert);
+    printChange(makeChange(toConvert),toConvert);
     return (0);
 }
 float * makeChange(float toConvert){
     float changeNumbers[] = {2,1,0.25,0.10,0.05,0.01};
+    int *ptr;//to loop through and change values of the array
+    ptr = changeNumbers;
     /*This array originally holds the numerical value for each different type of coin.
      * As the program progress's the values are replaced with how much of that type of change will be used.
+     * toConvert will also be updated as the program progress's being filled with the current number of change left to be distributed.
      */
-    for(int i)
+    for(int i = 0; i <6; i++){\
+        if(toConvert != 0){
+        int temp = (int)toConvert/(*ptr+i);
+        toConvert = fmod(toConvert,*ptr+i);
+        *ptr = temp;
+        }
+    else{
+        i = 10;//leave the loop
+    }
+    }
+    return changeNumbers;
+}
+void printChange(int numbers[5],float totalChange){
+    printf("Your change of %f consists of: \n", totalChange);
+    char *names[] = {
+      "Twoonie",
+      "Loonie",
+      "Quarter",
+      "Dime",
+      "Nickel",
+      "Penny"
+   };
+    for(int i = 0; i <6; i++){
+        if(numbers[i]!=0){//only print out the number of coins they receive 1 or more back of
+            printf("%s : %i$",names[i], numbers[i]);
+        }
+    }
+        
+    
 }
 
